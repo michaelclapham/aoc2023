5227b565ee563d61aff8cddcfbfd2a7ea6cc841a
Michael Clapham <michaeljclapham@gmail.com>
2023-12-06 21:24:56 +0000

    Lots of assembly learning progress

---
+++ day1/old/2.s
@@ -0,0 +1,18 @@
+extern printf
+extern exit
+
+section .data
+    msg DD "Hello World!",0
+    msg2 DD "Hello... again",0
+    fmt DB "Output is: %s %s",10,0
+section .text
+
+global main
+
+main:
+    PUSH msg2
+    PUSH msg
+    PUSH fmt
+    CALL printf
+    PUSH 1
+    CALL exit
--- day1/old/3.s
+++ day1/old/3.s
@@ -2,12 +2,10 @@ extern printf
 extern exit
 
 section .data
-    loopFmt DB "loop iteration %i",10,0
     fmt DB "Input is:",10,"%s",10,0
     pathname DD "./input.txt"
-    list DB 1,2,3,4
 section .bss
-    buffer: resb 64
+    buffer: resb 1024
 section .text
 
 global main
@@ -23,7 +21,7 @@ main:
     MOV ebx,eax
     MOV eax,3
     MOV ecx,buffer
-    MOV edx,64
+    MOV edx,1024
     INT 80h
 
     ; Print test message
@@ -31,19 +29,6 @@ main:
     PUSH fmt
     CALL printf
 
-    ; Test loop
-    MOV eax, 0
-loop1:
-    PUSH eax
-    PUSH loopFmt
-    CALL printf
-    INC eax
-    CMP eax,3
-    JGE end
-    JMP loop1
-
-end:
     ; Exit the program
-    MOV eax,1
-    MOV ebx,1
-    INT 80h
+    PUSH 1
+    CALL exit
