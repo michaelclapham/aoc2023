63a812355d57816ca21f8adae98eeed18d0013ed
Michael Clapham <michaeljclapham@gmail.com>
2023-12-06 22:21:31 +0000

    Working loop

--- day1/first.s
+++ day1/first.s
@@ -1,49 +1,18 @@
-extern printf
-extern exit
-
 section .data
-    loopFmt DB "loop iteration %i",10,0
-    fmt DB "Input is:",10,"%s",10,0
-    pathname DD "./input.txt"
     list DB 1,2,3,4
-section .bss
-    buffer: resb 64
 section .text
 
 global main
 
 main:
-    ; Open file
-    MOV eax,5
-    MOV ebx,pathname
-    MOV ecx,0
-    INT 80h
-
-    ; Read file into buffer
-    MOV ebx,eax
-    MOV eax,3
-    MOV ecx,buffer
-    MOV edx,64
-    INT 80h
-
-    ; Print test message
-    PUSH buffer
-    PUSH fmt
-    CALL printf
-
-    ; Test loop
     MOV eax, 0
-loop1:
-    PUSH eax
-    PUSH loopFmt
-    CALL printf
+    MOV cl, 0
+loop:
+    MOV bl,[list+eax]
+    ADD cl,bl
     INC eax
-    CMP eax,3
-    JGE end
-    JMP loop1
-
+    CMP eax,4
+    JE end
+    JMP loop
 end:
-    ; Exit the program
-    MOV eax,1
-    MOV ebx,1
     INT 80h
