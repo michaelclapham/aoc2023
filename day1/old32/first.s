65b5df496423cef9dfe367b5e5605b74b25d83f7
Michael Clapham <michaeljclapham@gmail.com>
2023-12-14 00:01:45 +0000

    Fix loop counter

--- day1/old32/first.s
+++ day1/old32/first.s
@@ -4,11 +4,15 @@ section .data
     startMsg DB "Start message",10,0
     newLineMsg DB "new line!",10,0
     digitMsg DB "digit!",10,0
+    firstDigitMsg DB "first digit!",10,0
+    secondDigitMsg DB "second digit!",10,0
     loopFmt DB "loop iteration %c",10,0
-    fmt DB "Input is:",10,"%s",10,0
+
     pathname DD "../sample_input.txt"
-    list DB 1,2,3,4
-    loopCounter DB 0
+
+    loopCounter DD 0
+
+    digitsOnLine DD 0
 section .bss
     buffer: resb 21464
 section .text
@@ -34,20 +38,21 @@ main:
     CALL printf
 
     ; Test loop
-    MOV edx, 0
-    MOV [loopCounter], edx
+    MOV ecx, 0
+    MOV [loopCounter], ecx
 loop1:
-    MOV edx, [loopCounter]
-    MOV ecx, [buffer + edx] ; ecx is now the current character in the string
+    MOV ecx, [loopCounter]
+    MOV edx, [buffer + ecx] ; ecx is now the current character in the string
 
     ; Print the character we're on
-    PUSH ecx
+    PUSH edx
     PUSH loopFmt
     CALL printf
+    MOV edx, 0
 
 check_for_newline:
-    MOV edx, [loopCounter]
-    MOV dl, [buffer + edx] ; dl is now the current character in the string
+    MOV ecx, [loopCounter]
+    MOV dl, [buffer + ecx] ; dl is now the current character in the string
     ; dl is 8 bit register which we are using to compare against ASCII numbers
     
     ; check if we are on a new line
@@ -56,28 +61,50 @@ check_for_newline:
 
 check_for_digit:
     CMP dl, 48
-    JLE loopEnd
+    JLE loopEnd ; if <= 48 then not a digit, end loop
 
-    CMP dl, 57
+    CMP dl, 57 ; if >= 57 then not a digit, end loop
     JGE loopEnd
 
     JMP onDigit
 
 onDigit:
-    PUSH digitMsg
+    MOV ecx, [digitsOnLine]
+    CMP ecx, 0 ; now check value before incrementing
+    JE onFirstDigit ; if = 1 then first digit
+    JMP onSecondDigit ; else second digit
+
+onFirstDigit:
+    MOV ecx, [digitsOnLine]
+    INC ecx
+    MOV [digitsOnLine], ecx
+    MOV ecx, 0
+
+    PUSH firstDigitMsg
+    CALL printf
+
+    
+    JMP loopEnd
+
+onSecondDigit:
+    PUSH secondDigitMsg
     CALL printf
     JMP loopEnd
 
 onNewLine:
     PUSH newLineMsg
     CALL printf
+
+    MOV ecx, 0
+    MOV [digitsOnLine], ecx
+
     JMP loopEnd
 
 loopEnd:
-    MOV edx, [loopCounter] ; move loop counter into edx so we can increment it
-    INC edx
-    MOV [loopCounter], edx ; write incremented edx back into loop counter
-    CMP edx,42 ; finish loop if loop counter >= 3
+    MOV ecx, [loopCounter] ; move loop counter into edx so we can increment it
+    INC ecx
+    MOV [loopCounter], ecx ; write incremented edx back into loop counter
+    CMP ecx,42 ; finish loop if loop counter >= 42
     JGE end
     JMP loop1
 
