9ac56384dfb10a6357c9307544ec13c512847bad
Michael Clapham <michaeljclapham@gmail.com>
2023-12-19 01:02:27 +0000

    Print hello world from arm assembly

--- day1/part2/part2.s
+++ day1/part2/part2.s
@@ -11,7 +11,15 @@ len = . - msg
 .global main
 
 main:
+
+    /* syscall write */
+    mov x0, #1 /* file descriptor 1 = standard out = console */
+    adr x1, msg
+    mov x2, #12 /* Number of bytes / characters in ascii string */
+    mov x8, #64 /* syscall 64 = write */
+    svc #0
+
     /* syscall exit(int status) */
     mov x0, #42 /* exit value is 42 */
-    mov x8, #93
+    mov x8, #93 /* syscall 93 = exit */
     svc #0
