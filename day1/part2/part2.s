97ddafe0d45421491e1298771dfad99d3aa4b0da
Michael Clapham <michaeljclapham@gmail.com>
2023-12-19 21:28:25 +0000

    Attempt open, read, close syscalls

--- day1/part2/part2.s
+++ day1/part2/part2.s
@@ -1,9 +1,28 @@
+/* Provides sys call number lookup */
+#include <asm/unistd.h>
+
+/* file io linux constants */
+.equ	O_RDONLY, 0
+.equ	O_WRONLY, 1
+.equ	O_CREAT,  0100
+.equ	S_RDWR,   0666
+.equ	AT_FDCWD, -100
+
+/* buffer size */
+.equ	BUFFER_SIZE, 250
+
 .data
 
 /* Data segment: define our message string and calculate its length. */
 msg:
     .ascii "Hello, ARM!\n"
 len = . - msg
+
+inputFilename:
+    .ascii "./sample_input.txt"
+
+buffer:
+    .fill BUFFERLEN + 1, 1, 0
 
 .text
 
@@ -12,12 +31,39 @@ len = . - msg
 
 main:
 
+    /* syscall openat */
+	mov x0, #AT_FDCWD // allows opening relative to current directory
+    ldr x1, =inputFilename
+    mov x2, #O_RDONLY
+	mov x3, #S_RDWR // mode param
+    mov x8, #__NR_openat
+    svc 0
+
+    mov x11, x0 // save file handle
+
+    /* syscall read */
+    mov x0, x11 // file descriptor
+    ldr x1, =buffer
+    mov x2, =BUFFERLEN
+    mov x8, #__NR_read
+    svc 0
+
     /* syscall write */
     mov x0, #1 /* file descriptor 1 = standard out = console */
     adr x1, msg
     mov x2, #12 /* Number of bytes / characters in ascii string */
     mov x8, #64 /* syscall 64 = write */
     svc #0
+
+    //fsync syscall
+    mov x0, x11 // holds input file handle
+    mov x8, #__NR_fsync
+    svc 0
+
+    //close syscall
+    mov x0, x11 // holds input file handle
+    mov x8, #__NR_close
+    svc 0
 
     /* syscall exit(int status) */
     mov x0, #42 /* exit value is 42 */
