53e8d85504c445edb823d471fbe200515367dec9
Michael Clapham <michael.clapham@guardian.co.uk>
2023-12-24 20:16:18 +0000

    Refactor using macros

--- day1/part2/part2.s
+++ day1/part2/part2.s
@@ -15,6 +15,34 @@ msg:
     .ascii "Hello, ARM!\n"
 len = . - msg
 
+oneStr:
+    .ascii "one"
+
+twoStr:
+    .ascii "two"
+
+threeStr:
+    .ascii "three"
+
+fourStr:
+    .ascii "four"
+
+fiveStr:
+    .ascii "five"
+
+sixStr:
+    .ascii "six"
+
+sevenStr:
+    .ascii "seven"
+
+eightStr:
+    .ascii "eight"
+
+nineStr:
+    .ascii "nine"
+
+
 inputFilename:
     .ascii "./sample_input.txt"
 
@@ -55,22 +83,73 @@ main:
     mov x8, #64 /* syscall 64 = write */
     svc #0
 
-    mov x2, #0 // input index
-    mov x3, #0 // output index
-
-loopHeader:
+    /* input address */
     ldr	x0, =inputBuffer // set x0 to inputBuffer start address
-    ldrb w5, [x0, x2] // load character / byte from address at x0 + x2 offset
-    
-checkOne:
-    mov x4, x2 // x4 is look-ahead address
-    // let's use w6 as the look-ahead char value
-    cmp w5, #111 // check if w5 is 'o'
+
+    /* output address */
+    ldr x6, =outputBuffer // set x6 to outputBuffer start address
+
+    mov x2, #0 // x2 is the look ahead offset. reset to zero
+
+.macro checkNum numStr, startLabel, loopLabel, nextLabel, replaceLabel, replaceChar, replaceLen
+
+\startLabel:
+    mov x2, #0 // x2 is the look ahead offset. reset to zero
+
+\loopLabel:
+    ldr	x1, =\numStr // set x1 to oneStr start address
+    ldrb w3, [x0, x2] // load lookahead from input at x0 + x2 offset into w3
+    ldrb w4, [x1, x2] // load number string from at x1 + x2 offset into w4
+    cmp w4, #0 // check if we've reached end of string
+    beq replaceLabel
+    cmp w3, w4 // check if w3 (lookahead on input) equals w4 (number string + offset)
+    bne \nextLabel
+    add x2, x2, #1 // increment offset
+    b \currentLabel
+
+\replaceLabel:
+    mov w3, \replaceChar
+    strb w3, [x6] // store character at w5 to address at x6 register (output pointer)
+    add x0, x0, \replaceLen
+    b loopFooter
+.endm
+
+searchAndReplace:
+
+checkNum oneStr checkOne loopOne checkTwo replaceOne #49 #3
+
+checkNum twoStr checkTwo loopTwo checkThree replaceTwo #50 #3
+
+checkNum threeStr checkThree loopThree checkFour replaceThree #51 #5
+
+checkNum fourStr checkFour loopFour checkFive replaceFour #52 #4
+
+checkNum fiveStr checkFive loopFive checkSix replaceFive #53 #4
+
+checkNum sixStr checkSix loopSix checkSeven replaceSix #54 #3
+
+checkNum sevenStr checkSeven loopSeven checkEight replaceSeven #55 #5
+
+checkNum eightStr checkEight loopEight checkNine replaceEight #56 #5
+
+checkNum nineStr checkNine loopNine loopFooter replaceNine #57 #4
+
+noNumber:
+    strb w3, [x0] // store character at w5 to address at x0 register (output pointer)
+    mov x0, x0, #1 // increment input pointer by 1 character
+    b loopFooter
+
+/* checkOne:
+    ldr	x1, =oneStr // set x1 to oneStr start address
+    mov x2, #0 // x2 is the look ahead offset. reset to zero
+    ldrb w3, [x0, x2] // load lookahead from input at x0 + x2 offset into w3
+    ldrb w4, [x1, x2] // load number string from at x1 + x2 offset into w4
+    cmp w3, w4 // check if w5 is 'o'
     bne checkTwo
     
-    add x4, x2, #1 // look to char at + x2 + 1
-    ldrb w6, [x0, x4] // load character / byte from address at x0 + x2 + 1 offset
-    cmp w6, #119 // check if w6 is 'n'
+    add x2, x2, #1 // increase lookahead by 1
+    ldrb w5, [x0, x2] // load character / byte from address at x0 + x2 offset
+    cmp w5, #119 // check if w6 is 'n'
     bne checkTwo
 
     add x4, x2, #2 // look to char at + x2 + 2
@@ -148,22 +227,16 @@ checkThree:
     mov w5, #50 // ASCII 50 = '2'.
     b loopFooter
 
+*/
+
 loopFooter:
-    // write x5 to output buffer at output offset (x3)
-    ldr x1, =outputBuffer // set x1 to outputBuffer start address
-    strb w5, [x1, x3] // store character at w5 to address at x1 + x3 offset
-
-    // increment both input and output positions always
-    add x2, x2, #1 // increment x2 by 1
-    add x3, x3, #1 // increment x3 by 1
-
     // Stop looping if loop counter higher than buffer size
     cmp x2, #BUFFER_SIZE
     bgt end
 
     cmp	w5, #0 // check if character is null character
 	beq end // end if character isn't null
-    b loopHeader
+    b searchAndReplace
 
 end:
 
