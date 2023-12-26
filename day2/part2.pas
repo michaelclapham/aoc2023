98b2d67d482bb2c615b708d0d1dcf1186a5cbf26
Michael Clapham <michael.clapham@guardian.co.uk>
2023-12-26 12:19:40 +0000

    Fix day2-part2 by using longInt. In free pascal it seems Integer defaults to smallInt (16 bit signed int) so has a max value of 32767 before overflowing to negative

--- day2/part2.pas
+++ day2/part2.pas
@@ -11,7 +11,7 @@ var
   turns, draws: TStringArray;
   turn, draw, dt: AnsiString;
   line: AnsiString;
-  i, num, power, powerTotal: Integer;
+  i, num, power, powerTotal: longInt;
   possible: Boolean;
 
 Begin
@@ -25,12 +25,12 @@ Begin
   while not Eof do  // Read until the end of input
   begin
     ReadLn(line);  // Read a line from standard input
-    WriteLn('You entered: ', line);  // Display the entered line
+    // WriteLn('You entered: ', line);  // Display the entered line
     // Extract game number and draw information
     gameStr := Copy(line, 1, Pos(':', line) - 1);
     gameNumStr := Trim(StringReplace(gameStr, 'Game ', '', [rfReplaceAll, rfIgnoreCase]));
     gameNum := StrToInt(gameNumStr);
-    WriteLn('Game num ', gameNum);
+    //WriteLn('Game num ', gameNum);
     after := Copy(line, Pos(':', line) + 2, Length(line));
 
     // For each line, there a turns, each turn consists of multiple draws
@@ -43,49 +43,49 @@ Begin
     turns := after.Split([';']);
     for turn in turns do
       begin
-        WriteLn('turn ', turn);
+        //WriteLn('turn ', turn);
         draws := turn.Split([',']);
 
         // Find maximum red, green, and blue counts
         for draw in draws do
         begin
-          WriteLn('draw ', draw);
+          //WriteLn('draw ', draw);
           if Pos('red', draw) > 0 then
           begin
             num := StrToInt(Trim(StringReplace(draw, 'red', '', [rfReplaceAll, rfIgnoreCase])));
-            WriteLn('red num ', num);
+            //WriteLn('red num ', num);
             if num > redMax then redMax := num;
           end;
           if Pos('green', draw) > 0 then
           begin
             num := StrToInt(Trim(StringReplace(draw, 'green', '', [rfReplaceAll, rfIgnoreCase])));
-            WriteLn('green num ', num);
+            //WriteLn('green num ', num);
             if num > greenMax then greenMax := num;
           end;
           if Pos('blue', draw) > 0 then
           begin
             num := StrToInt(Trim(StringReplace(draw, 'blue', '', [rfReplaceAll, rfIgnoreCase])));
-            WriteLn('blue num ', num);
+            //WriteLn('blue num ', num);
             if num > blueMax then blueMax := num;
           end;
         end;
       end;
-    WriteLn('red max ', redMax);
-    WriteLn('green max ', greenMax);
-    WriteLn('blue max ', greenMax);
+    //WriteLn('red max ', redMax);
+    //WriteLn('green max ', greenMax);
+    //WriteLn('blue max ', greenMax);
 
     // Check if constraints are met
     possible := (redMax <= redConstraint) and (greenMax <= greenConstraint) and (blueMax <= blueConstraint);
 
     if possible then
     begin
-      WriteLn('game is possible: ', gameNum);
+      //WriteLn('game is possible: ', gameNum);
       total := total + gameNum;
     end;
 
     power := Max(redMax,1) * Max(greenMax,1) * Max(blueMax,1);
 
-    WriteLn('power for game num ', gameNum, ' is ', power);
+    WriteLn('game num ', gameNum, ' power: ', power, ' power total before: ', powerTotal);
 
     powerTotal := powerTotal + power;
 
